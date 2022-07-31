090dd6a5321a3e7de415cdb3ec34289506c6b588
nhut_pham <nhut.pham@ilogic.vn>
2022-07-31 12:53:31 +0700

    config eureka client

--- src/main/java/core/microservices/config/client/api/LiveApi.java
+++ src/main/java/core/microservices/config/client/api/LiveApi.java
@@ -3,16 +3,20 @@ package core.microservices.config.client.api;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import core.microservices.config.client.ProjectConfig;
 import core.microservices.config.client.dto.ProjectConfigDto;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 public class LiveApi {
 
@@ -23,7 +27,11 @@ public class LiveApi {
 	private ProjectConfig projectConfig;
 	
 	@GetMapping("/live-check")
-	public String liveCheck() throws Exception {
+	public String liveCheck(@RequestParam(required = false) Integer sleep) throws Exception {
+		log.info("liveCheck:: " + sleep);
+		if(sleep != null) {
+			Thread.sleep(sleep*1000);
+		}
 		Integer port = Integer.parseInt(environment.getProperty("server.port"));
 		return String.format("Config Client Server:: %s", port);
 	}
@@ -40,5 +48,29 @@ public class LiveApi {
 	@GetMapping("/configuration")
 	public ProjectConfigDto configuration() throws Exception {
 		return new ProjectConfigDto(projectConfig);
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		for(int i=1; i<=5; i++) {
+			makeHelloCall(i+"", 3);
+		}
+		Thread.sleep(2000000);
+	}
+	
+	private static void makeHelloCall(String name, Integer sleep) {
+		new Thread(() -> {
+			int callCount = 0;
+			while(true) {
+				callCount++;
+				String response = new RestTemplate()
+						.getForObject("http://localhost:8762/hello?sleep="+sleep+"&name="+name, String.class);
+				System.out.println("response:: " + name + " - " + callCount + " - " + response);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
 	}
 }
